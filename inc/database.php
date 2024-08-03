025aaec9a05a67b09302f15ce91ec2f976271670
Williams Chukwuma <WilliamsChukwuma@users.noreply.github.com>
2024-08-03 09:21:43 -0400

    second commit

--- inc/database.php
+++ inc/database.php
@@ -1,9 +1,13 @@
 <?php
-	try{
-		$conn = new PDO('mysql:host=localhost;dbname=mydb','root','');
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch(PDOException $e){
-		echo "Connection failed: " . $e->getMessage();
-	}
+$server = "sql105.infinityfree.com"; 
+$username = "if0_36995341";
+$password = "YXaAY2HBAKe6pG";
+$dbname = "if0_36995341_williamscode";
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
--- sql/assignment2.sql
+++ sql/assignment2.sql
@@ -1,13 +1,13 @@
-create  table mydb.user(
+create  table if0_36995341_williamscode.user(
     userID_Number int not null auto_increment,
 	fullname varchar (255),
 	Email varchar (255) NOT NULL UNIQUE,
 	Gender varchar (10),
 	password varchar (255),
-    primary key (userID_Number)
+    primary key (userID_Number),
 );
 
-CREATE TABLE uploadImages (
+CREATE TABLE if0_36995341_williamscode.uploadImages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
